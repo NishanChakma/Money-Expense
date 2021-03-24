8107df608c191163ac0432f5f0e77e598997f002
Nishan IT <nishan@m.insta.kim>
2021-03-24 16:18:13 +0600

    all expenses

--- src/components/allExpense/AllExpenseComponent.js
+++ src/components/allExpense/AllExpenseComponent.js
@@ -4,42 +4,84 @@ import styles from './styles';
 import {connect} from 'react-redux';
 import {v4 as uuidv4} from 'uuid';
 import Items from '../home/Items';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
 
 const AllExpenseComponent = props => {
-  const [startDate, setStartDate] = useState('Select Start Date');
-  const [endDate, setEndDate] = useState('Select End Date');
+  const data = props.Expense ? props.Expense : [];
   const key = () => uuidv4();
-  const renderItem = useCallback(({item}) => <Items item={item} />, [
-    props.Expense,
-  ]);
+  const renderItem = useCallback(({item}) => <Items item={item} />, [data]);
+  //filter task will be in here
+
+  // -------------------date time start-------------
+  const [startTimeStamp, setstartTimeStamp] = useState(0);
+  const [endTimeStamp, setEndTimeStamp] = useState(0);
+  const [selectBox, setSelectBox] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const showDatepicker = useCallback(
+    param => {
+      param === 0 ? setSelectBox(0) : setSelectBox(1);
+      setShow(true);
+    },
+    [selectBox, show],
+  );
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate ? selectedDate : new Date();
+    let timestamp = moment(currentDate).unix();
+    selectBox === 0 ? setstartTimeStamp(timestamp) : setEndTimeStamp(timestamp);
+    setShow(Platform.OS === 'ios');
+  };
+
+  // -------------------date time end-------------
+
   return (
     <View style={styles.container}>
       <View style={styles.selectBox}>
-        <TouchableOpacity style={styles.selectBoxInside} onPress={null}>
-          <Text style={styles.text}>{startDate}</Text>
+        <TouchableOpacity
+          style={styles.selectBoxInside}
+          onPress={() => showDatepicker(0)}>
+          <Text style={styles.text}>
+            {startTimeStamp != 0
+              ? moment.unix(startTimeStamp).format('D/M/YYYY')
+              : 'Select Start Date'}
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.selectBoxInside} onPress={null}>
-          <Text style={styles.text}>{endDate}</Text>
+        <TouchableOpacity
+          style={styles.selectBoxInside}
+          onPress={() => showDatepicker(1)}>
+          <Text style={styles.text}>
+            {endTimeStamp != 0
+              ? moment.unix(endTimeStamp).format('D/M/YYYY')
+              : 'Select End Date'}
+          </Text>
         </TouchableOpacity>
       </View>
       <FlatList
         showsVerticalScrollIndicator={false}
-        data={props.Expense}
+        data={data}
         renderItem={renderItem}
         keyExtractor={key}
       />
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={new Date()}
+          mode={'date'}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
 
 const mapStateToProps = state => {
-  const TotalAmount = state.ExpenseReducer.totalAmount;
   const Expense = state.ExpenseReducer.expense;
-  const Loading = state.ExpenseReducer.loading;
   return {
-    TotalAmount,
     Expense,
-    Loading,
   };
 };
 
